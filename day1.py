e2cb8182bb58185fce74332b61d50a0407f5aeea
khasimvali-dev <sskhasimvali@gmail.com>
2026-07-19 14:58:49 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,13 @@
+name = "Khasim Vali"
+age = 21
+college = "G Pulla Reddy Engineering College Kurnool"
+branch = "Civil Engineering"
+cgpa = 8.02
+dream_job = "Python Full Stack Developer"
+
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+print("Branch:", branch)
+print("CGPA:", cgpa)
+print("Dream Job:", dream_job)
